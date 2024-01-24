72d821d826f118fe49eb31136087892f18e16cb3
lans <>
2024-01-24 13:54:39 +0000

    Dont run linter/formatter if not installed

--- precommit.sh
+++ precommit.sh
@@ -2,12 +2,29 @@
 
 set -euo pipefail
 
-find -iname '*go' -print -exec gofmt -s -w {} \;
+if command -v gofmt
+then
+  find -iname '*go' -print -exec gofmt -s -w {} \;
+else
+  echo "Not doing gofmt (gofmt not found)"
+fi
 
 python_files="$( find -iname '*py' )"
-black $python_files
-pylint --argument-naming-style camelCase \
-       --attr-naming-style camelCase \
-       --variable-naming-style camelCase \
-       --function-naming-style camelCase \
-       $python_files
+
+if command -v black
+then
+  black $python_files
+else
+  echo "Not doing autoformat (black not found)"
+fi
+
+if command -v pylint
+then 
+  pylint --argument-naming-style camelCase \
+         --attr-naming-style camelCase \
+         --variable-naming-style camelCase \
+         --function-naming-style camelCase \
+         $python_files
+else
+  echo "Not doing static analysis (pylint not found)"
+fi
